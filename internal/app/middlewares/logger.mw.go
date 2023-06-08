a65a5bb6865aa019361ad107064e9314ce5d0418
vergil0327 <vergil.wang@relajet.com>
2023-06-08 16:26:49 +0800

    feat(middleware): add logger middleware

---
+++ internal/app/middlewares/logger.mw.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"boilerplate/internal/pkg/logger"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		path := c.Request.URL.Path
+
+		start := time.Now()
+		fields := make(map[string]interface{})
+		fields["ip"] = c.ClientIP()
+		fields["method"] = c.Request.Method
+		fields["url"] = c.Request.URL.String()
+		fields["protocol"] = c.Request.Proto
+		fields["header"] = c.Request.Header
+		fields["user_agent"] = c.Request.UserAgent()
+		fields["content_length"] = c.Request.ContentLength
+
+		c.Next()
+
+		timeConsuming := time.Since(start).Nanoseconds() / 1e6
+		fields["res_status"] = c.Writer.Status()
+		fields["res_length"] = c.Writer.Size()
+
+		ctx := c.Request.Context()
+		entry := logger.WithContext(logger.NewTagContext(ctx, "__request__"))
+		entry.
+			WithFields(fields).
+			Infof("[http] %s-%s-%s-%d(%dms)", path, c.Request.Method, c.ClientIP(), c.Writer.Status(), timeConsuming)
+	}
+}
